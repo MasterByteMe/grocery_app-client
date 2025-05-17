57fa5817f0558a3e5d5dc2483e4d79f7562fb985
Anklebracelet24 <mark.devz.08@gmail.com>
2025-05-17 10:14:04 +0800

    first commit

---
+++ src/context/AppContext.jsx
@@ -0,0 +1,127 @@
+import {
+  Component,
+  createContext,
+  useContext,
+  useState,
+  useEffect,
+} from "react";
+import { useNavigate } from "react-router-dom";
+import { dummyProducts } from "../assets/assets";
+import toast from "react-hot-toast";
+
+export const AppContext = createContext();
+
+const currency = import.meta.VITE_CURRENCY;
+
+// return contextprovider tag
+export const AppContextProvider = ({ children }) => {
+  const navigate = useNavigate();
+  //identify the user - Navbar.jsx
+  const [user, setUser] = useState(null);
+
+  const [isSeller, setIsSeller] = useState(false);
+  // use to display and hide login navmenu
+  const [showUserLogin, setShowUserLogin] = useState(false);
+
+  // state for products - ProductCard.jsx
+  const [products, setProducts] = useState([]);
+
+  //for (+,-) on Card ProductCard.jsx
+  const [cartItems, setCartItems] = useState({});
+
+  const [searchQuery, setSearchQuery] = useState({});
+
+  // Function that will fetch all the products from the assets
+  const fectchProducts = async () => {
+    setProducts(dummyProducts);
+  };
+
+  // Add Product to Cart - ProductCard.jsx
+  const addToCart = (itemId) => {
+    let cartData = structuredClone(cartItems);
+
+    if (cartData[itemId]) {
+      cartData[itemId] += 1;
+    } else {
+      cartData[itemId] = 1;
+    }
+    setCartItems(cartData);
+    toast.success("Added to Cart");
+  };
+
+  // Update Cart Item Quantity - ProductCard.jsx
+  const updateCartItem = (itemId, quantity) => {
+    let cartData = structuredClone(cartItems);
+    cartData[itemId] = quantity;
+    setCartItems(cartData);
+    toast.success("Cart Updated");
+  };
+
+  // Remove Product from Cart - ProductCard.jsx
+  const removeFromCart = (itemId) => {
+    let cartData = structuredClone(cartItems);
+    if (cartData[itemId]) {
+      cartData[itemId] -= 1;
+      if (cartData[itemId] === 0) {
+        delete cartData[itemId];
+      }
+    }
+    toast.success("Remove from Cart");
+    setCartItems(cartData);
+  };
+
+  // Get Cart Item Count
+  // Calculate the total number of items in the cart, then return the totalCount of the product added in the cart
+  const getCartCount = () => {
+    let totalCount = 0;
+    for (const item in cartItems) {
+      totalCount += cartItems[item];
+    }
+    return totalCount;
+  };
+
+  // Get Cart Total Amount
+  const getCartAmount = () => {
+    let totalAmount = 0;
+    for (const items in cartItems) {
+      let itemInfo = products.find((product) => product._id === items);
+      if (cartItems[items] > 0) {
+        totalAmount += itemInfo.offerPrice * cartItems[items];
+      }
+    }
+    return Math.floor(totalAmount * 100) / 100;
+  };
+
+  // function works when the page loaded
+  useEffect(() => {
+    fectchProducts();
+  }, []);
+
+  //access the states variable function in any other component
+  const value = {
+    navigate,
+    user,
+    setUser,
+    isSeller,
+    setIsSeller,
+    showUserLogin,
+    setShowUserLogin,
+    products,
+    currency,
+    addToCart,
+    updateCartItem,
+    removeFromCart,
+    cartItems,
+    searchQuery,
+    setSearchQuery,
+    getCartAmount,
+    getCartCount,
+  };
+
+  return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
+};
+
+// use to other component and we can directly access the data stored in "value" object
+export const useAppContext = () => {
+  return useContext(AppContext);
+};
---
+++ src/pages/AddAddress.jsx
@@ -0,0 +1,141 @@
+import React, { useState } from "react";
+import { assets } from "../assets/assets";
+
+// Input Field Component
+const InputField = ({ type, placeholder, name, handleChange, address }) => (
+  <input
+    className="w-full px-2 py-2.5 border border-gray-500/30 rounded outlline-none text-gray-500 focus:border-primary transition"
+    type={type}
+    placeHolder={placeholder}
+    onChange={handleChange}
+    name={name}
+    value={address[name]}
+    required
+  />
+);
+
+const AddAddress = () => {
+  const [address, setAddress] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    street: "",
+    city: "",
+    state: "",
+    zipcode: "",
+    country: "",
+    phone: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    setAddress((prevAddress) => ({
+      ...prevAddress,
+      [name]: value,
+    }));
+  };
+
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className="mt-16 pb-16">
+      <p className="text-2xl md:text-3xl text-gray-500">
+        Add Shipping <span className="font-semibold text-primary">Address</span>
+      </p>
+      <div className="flex flex-col-reverse md:flex-row justify-between mt-10">
+        <div className="flex-1 max-w-md">
+          <form onSubmit={onSubmitHandler} className="space-y-3 mt-6 text-sm">
+            <div className="grid grid-cols-2 gap-4">
+              <InputField
+                handleChange={handleChange}
+                address={address}
+                name="firstName"
+                type="text"
+                placeholder="first Name"
+              />
+
+              <InputField
+                handleChange={handleChange}
+                address={address}
+                name="lastName"
+                type="text"
+                placeholder="last Name"
+              />
+            </div>
+            <InputField
+              handleChange={handleChange}
+              address={address}
+              name="email"
+              type="email"
+              placeholder="Email Address"
+            />
+
+            <InputField
+              handleChange={handleChange}
+              address={address}
+              name="street"
+              type="email"
+              placeholder="Street"
+            />
+
+            <div className="grid grid-cols-2 gap-4">
+              <InputField
+                handleChange={handleChange}
+                address={address}
+                name="city"
+                type="email"
+                placeholder="City"
+              />
+              <InputField
+                handleChange={handleChange}
+                address={address}
+                name="state"
+                type="text"
+                placeholder="State"
+              />
+            </div>
+
+            <div className="grid grid-cols-2 gap-4">
+              <InputField
+                handleChange={handleChange}
+                address={address}
+                name="zipcode"
+                type="number"
+                placeholder="Zip code"
+              />
+              <InputField
+                handleChange={handleChange}
+                address={address}
+                name="country"
+                type="text"
+                placeholder="Country"
+              />
+            </div>
+
+            <InputField
+              handleChange={handleChange}
+              address={address}
+              name="phone"
+              type="text"
+              placeholder="Phone"
+            />
+
+            <button className="w-full mt-6 bg-primary text-white py-3 hover:bg-primary-dull transition cursor-pointer uppercase">
+              Save Address
+            </button>
+          </form>
+        </div>
+        <img
+          className="md:mr-16 mb-16 md:mt-0"
+          src={assets.add_address_iamge}
+          alt="Add Address"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default AddAddress;
